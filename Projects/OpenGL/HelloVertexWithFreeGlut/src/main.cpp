1e4ca2f79689d2942b2135ca51b864a75df4e505
BadLagger <lanynamer1985@gmail.com>
2020-07-07 06:37:13 +0300

    Проект с использованием freeglut

---
+++ Projects/OpenGL/HelloVertexWithFreeGlut/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "freeglut.h"
+
+int WINAPI WinMain( HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow )
+{
+	glutInitContextVersion( 3, 3 );
+	return 0;
+}
